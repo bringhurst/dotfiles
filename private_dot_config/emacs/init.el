d4b2023f7417c106e0f506c8bf0805196f9c32b4
Jon Bringhurst <jonb@haven.internal>
2025-03-16 15:41:31 -0700

    Add eglot

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -1,1 +1,20 @@
-; Init file placeholder
+;; first, declare repositories
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
+
+;; Init the package facility
+(require 'package)
+(package-initialize)
+;; (package-refresh-contents) ;; this line is commented 
+;; since refreshing packages is time-consuming and should be done on demand
+
+;; Declare packages
+(setq my-packages
+      '(eglot))
+
+;; Iterate on packages and install missing ones
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
